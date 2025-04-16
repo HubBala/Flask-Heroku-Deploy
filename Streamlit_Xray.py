b2cc47ffd4ca439d1f7a3eb67ada2839a6b8b837
Bala Bhaskar <balabhaskarnaraharisetti@gmail.com>
2025-04-17 03:31:42 +0530

    ui is improved

--- Streamlit_Xray.py
+++ Streamlit_Xray.py
@@ -18,27 +18,27 @@ uploaded_file = st.file_uploader("X-Ray Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     st.write("File uploaded", uploaded_file.name)
-try:
-     # Open and display the image
-    img = Image.open(uploaded_file).convert('RGB')
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    try:
+        # Open and display the image
+        img = Image.open(uploaded_file).convert('RGB')
+        st.image(img, caption="Uploaded Image", use_column_width=True)
 
-    # Preprocess the image
-    img = img.resize((150, 150))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+        # Preprocess the image
+        img = img.resize((150, 150))
+        img_array = np.array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
 
-    st.write(f"Image shape before prediction: {img_array.shape}")
+        st.write(f"Image shape before prediction: {img_array.shape}")
 
-    # Make prediction
-    prediction = model.predict(img_array)[0][0]
-    st.write("Raw Prediction:", prediction)
+        # Make prediction
+        prediction = model.predict(img_array)[0][0]
+        st.write("Raw Prediction:", prediction)
 
-    # Show result
-    if prediction > 0.5:
-        st.success("Prediction: Normal")
-    else:
-        st.error("Prediction: Pneumonia")
+        # Show result
+        if prediction > 0.5:
+            st.success("Prediction: Normal")
+        else:
+            st.error("Prediction: Pneumonia")
 
-except Exception as e:
-    st.error(f"Error opening image: {e}")
+    except Exception as e:
+        st.error(f"Error opening image: {e}")
